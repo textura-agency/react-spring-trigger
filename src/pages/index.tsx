5ea4937f07692f6e26e163522bb1b246854df137
Den Kravchu <denkravchu@gmail.com>
2024-02-19 21:01:23 +0700

    feat: update trigger

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,55 @@
+import { useSpringTrigger } from "@/useSpringTrigger/useSpringTrigger"
+import { useRef } from "react"
+import { animated } from "@react-spring/web"
+
+const styles: any = {
+    container: {
+        background: 'rgba(255, 0, 0, .5)',
+        width: '300px',
+        height: '2000px',
+        margin: '1500px 0'
+    },
+    div: {
+        top: '50%',
+        left: 'calc(50% - 150px)',
+        width: '300px',
+        height: '5px',
+        background: 'black',
+        position: 'fixed',
+    },
+    placeholder: {
+        top: '50%',
+        left: 'calc(50% - 150px)',
+        width: '300px',
+        height: '5px',
+        background: 'red',
+        opacity: '.2',
+        position: 'fixed',
+    }
+}
+
 export default function Home() {
+    const triggerRef = useRef<HTMLDivElement | null>(null)
+    const { values } = useSpringTrigger({
+        trigger: triggerRef,
+        start: 'bottom bottom',
+        end: 'bottom top',
+        scrub: false,
+        from: {
+          x: '100%'
+        },
+        to: {
+          x: '0%'
+        },
+        onChange: (state) => {
+          console.log(state)
+        }
+    })
     return (
         <>
-            <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-
+            <div ref={triggerRef} style={styles.container}>
+                <div style={styles.placeholder}></div>
+                <animated.div style={{...values, ...styles.div}}></animated.div>
             </div>
         </>
     )
--- src/useSpringTrigger/useSpringTrigger.ts
+++ src/useSpringTrigger/useSpringTrigger.ts
@@ -5,7 +5,7 @@ import { useSpring, SpringValues, SpringProps } from '@react-spring/web'
 export type TriggerPos = 'top top' | 'center top' | 'bottom top' | 'top center' | 'center center' | 'bottom center' | 'top bottom' | 'center bottom' | 'bottom bottom'
 
 export interface UseScrollOptions extends SpringProps {
-    trigger?: MutableRefObject<HTMLElement>,
+    trigger?: MutableRefObject<HTMLElement | HTMLDivElement | null>,
     start?: TriggerPos,
     end?: TriggerPos,
     from?: {[x: string]: any},
@@ -18,36 +18,25 @@ export interface ScrollState {
     scrollStart: number,
     scrollEnd: number,
     progress: number,
+    allProgress: number,
     length: number
 }
 export interface ScrollValues {
     [x: number]: string
 }
 
+export type ToggleActionTypes = "play" | "pause" | "resume" | "reset" | "restart" | "complete" | "reverse" | "none"
+
 /**
- * A small utility abstraction around our signature useSpring hook. It's a great way to create
- * a scroll-linked animation. With either the raw value of distance or a 0-1 progress value.
- * You can either use the scroll values of the whole document, or just a specific element.
- *
- * 
- ```jsx
-    import { useScroll, animated } from '@react-spring/web'
-
-    function MyComponent() {
-      const { scrollYProgress } = useScroll()
-
-      return (
-        <animated.div style={{ opacity: scrollYProgress }}>
-          Hello World
-        </animated.div>
-      )
-    }
-  ```
  * 
  * @param {UseScrollOptions} useScrollOptions options for the useScroll hook.
  * @param {MutableRefObject<HTMLElement>} useScrollOptions.trigger the container to listen to scroll events on, defaults to the window.
  * @returns {SpringValues<{progress: number;}>} SpringValues the collection of values returned from the inner hook
  */
+
+const defaultConfig = { mass: 1, tension: 170, friction: 26 }
+const defaultToggleAction = "play none none none"
+
 export const useSpringTrigger = ({
     trigger,
     start,
@@ -60,6 +49,11 @@ export const useSpringTrigger = ({
     ...springOptions
 }: UseScrollOptions = {}): {values: SpringValues<ScrollValues>, state: SpringValues<ScrollState> } => {
     const savers = useRef<any>({})
+
+    // Fix mount issue with wrong values in spring
+    const mounted = useRef(false) 
+    // 
+
     // For Values
     const [scrollValues, api] = useSpring(
         () => ({
@@ -75,10 +69,13 @@ export const useSpringTrigger = ({
     // For State
     const [scrollStateValues, stateApi] = useSpring(
         () => ({
-            progress: 0,
-            length: 0,
-            scrollStart: 0,
-            scrollEnd: 0,
+            from: {
+                progress: 0,
+                length: 0,
+                scrollStart: 0,
+                scrollEnd: 0,
+                allProgress: 0
+            },
             onChange
         }),
         []
@@ -88,12 +85,23 @@ export const useSpringTrigger = ({
         const cleanupScroll = onScroll(
             // @ts-expect-error
             ( state: ScrollState , values: ScrollValues ) => {
+                if (!mounted.current) {
+                    mounted.current = true
+                    savers.current = { ...savers.current, progress: state.progress }
+                    if (typeof state.progress !== 'number') { return console.error('[useTriggerScroll]: Invalid <start> or <end> provided') }
+                    setTimeout(() => {
+                        api.start({ to: { ...values }, config: { duration: 0 } })
+                        stateApi.start({ to: { ...state }, config: { duration: 0 } })
+                    }, 30)
+                    return
+                }
                 if (savers.current?.progress !== state?.progress) {
                     savers.current = { ...savers.current, progress: state.progress }
                     if (typeof state.progress !== 'number') { return console.error('[useTriggerScroll]: Invalid <start> or <end> provided') }
-                    api.start({ ...values })
-                    stateApi.start({ ...state })
+                    api.start({ to: { ...values }, config: springOptions?.config || defaultConfig })
+                    stateApi.start({ to: { ...state }, config: springOptions?.config || defaultConfig })
                 }
+
             },
             { trigger: trigger, start, end, from, to, scrub, toggleAction}
         )
@@ -125,7 +133,7 @@ export const onScroll = (
         const _trigger = trigger?.current || document.documentElement
         const state = calcProgress(start, end, _trigger)
         // @ts-expect-error
-        const values = calcValues(from, to, state.progress, scrub, toggleAction)
+        const values = calcValues(from, to, state.progress, state.allProgress, scrub, toggleAction)
         callback({ ...state }, { ...values })
         rq = requestAnimationFrame(render)
     }
@@ -142,6 +150,7 @@ function clamp(min: number, max: number, value: number) {
 function lerp(start: number, end: number, t: number) {
     return (1 - t) * start + t * end
 }
+
 
 function calcProgress(start: TriggerPos, end: TriggerPos, trigger: HTMLElement): ScrollState | {} {
     const bb = trigger.getBoundingClientRect()
@@ -168,18 +177,19 @@ function calcProgress(start: TriggerPos, end: TriggerPos, trigger: HTMLElement):
     const scrollEnd = poses[end.split(' ').join('_')]
     if (!scrollStart || !scrollEnd) { return {} }
     const length = Math.abs(scrollStart - scrollEnd)
-    const progress = 1 - clamp(0, 1, (scrollStart + length) / length)
-
+    const allProgress = (scrollStart + length) / length
+    const progress = 1 - clamp(0, 1, allProgress)
 
     return {
         scrollStart,
         scrollEnd,
         progress,
+        allProgress,
         length
     }
 }
 
-function calcValues(from: any = {}, to: any = {}, progress: number, scrub: boolean, toggleAction: any) {
+function calcValues(from: any = {}, to: any = {}, progress: number, allProgress: number, scrub: boolean, toggleAction: any) {
     const froms = Object.keys(from)
     const toes = Object.keys(to)
     const values: any = {}
@@ -209,7 +219,7 @@ function calcValues(from: any = {}, to: any = {}, progress: number, scrub: boole
         if (scrub) {
             values[value] = lerp(start, end, progress) + unit
         } else {
-            values[value] = getToggledValue(start, end, progress, toggleAction) + unit
+            values[value] = getToggledValue(start, end, progress, allProgress, toggleAction) + unit
         }
     })
     return values
@@ -217,23 +227,62 @@ function calcValues(from: any = {}, to: any = {}, progress: number, scrub: boole
 
 // TODO: Complete this
 // Now by default it works like 'play reset play reset'
-function getToggledValue(start: number, end: number, progress: number, toggleAction: any = "play reset play reset") {
-    // const actions = toggleAction.split(" ");
-    // const lastAction = actions[actions.length - 1];
+// onEnter, onLeave, onEnterBack, and onLeaveBack
+function validateActions(_actions: Array<ToggleActionTypes>) {
+    const actions = ["play", "pause", "resume", "reset", "restart", "complete", "reverse", "none"]
+    return _actions.filter(item => actions.find(_ => _ === item)).length === 4
+}
+function getToggledValue(start: number, end: number, progress: number, allProgress: number, toggleAction: any = defaultToggleAction) {
 
-    // if (lastAction === "play") {
-    //     return progress >= 1 ? end : start;
-    // } else if (lastAction === "pause") {
-    //     return progress >= 1 ? end : start;
-    // } else if (lastAction === "resume") {
-    //     return progress >= 1 ? end : start;
-    // } else if (lastAction === "reset") {
-    //     return progress >= 1 ? start : end;
-    // } else {
-    //     return progress >= 1 ? end : start;
+    // onEnter, onLeave, onEnterBack, and onLeaveBack
+    // "play", "pause", "resume", "reset", "restart", "complete", "reverse", and "none"
+    // Only "play" & "none"
+    let actions = toggleAction.split(" ");
+    if (!validateActions(actions)) {
+        console.warn('[useSpringTrigger]: wrong toggleActions provided')
+        actions = defaultToggleAction
+    }
+
+    // onEnter
+    // if (progress > 0 && progress < 1) {
+    //     if (actions[0] === 'play') {
+    //         return end
+    //     }
+    //     if (actions[0] === 'none') {
+    //         return start
+    //     }
+    // }
+    // // onLeave
+    // if (progress >= 1) {
+    //     if (actions[1] === 'play') {
+    //         return start
+    //     }
+    //     if (actions[1] === 'none') {
+    //         return end
+    //     }
+    // }
+    // // onEnterBack
+    // if (progress > 0 && progress < 1) {
+    //     if (actions[2] === 'play') {
+    //         return end
+    //     }
+    //     if (actions[2] === 'none') {
+    //         return start
+    //     }
+    // }
+    // // onLeaveBack
+    // if (progress <= 0) {
+    //     if (actions[3] === 'play') {
+    //         return start
+    //     }
+    //     if (actions[3] === 'none') {
+    //         return end
+    //     }
     // }
 
-    if (progress > 0 && progress < 1) {
+    // console.log(allProgress )
+
+    if (progress > 0) {
         return end
     }
     return start
